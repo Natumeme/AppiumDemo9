0ca57c56e1ac6cacb677829160166159dc603670
Natumeme <1466510866@qq.com>
2019-07-26 00:24:43 +0800

    add webview

---
+++ test_xueqiu_webviewhome.py
@@ -0,0 +1,73 @@
+#!usr/bin/env python
+#-*- coding:utf-8 -*-
+
+from appium import webdriver
+import pytest
+from appium.webdriver.common.mobileby import MobileBy
+from appium.webdriver.webdriver import WebDriver
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
+
+
+class TestXueqiuAndroid(object):
+	driver=WebDriver
+	@classmethod
+	def setup_class(cls):
+		print("setup class 在当前类下的所有用例执行之前只执行一次")
+		cls.driver=cls.restart_app()
+		WebDriverWait(cls.driver, 20).until(EC.presence_of_element_located((MobileBy.XPATH, "//*[@text='交易']")))
+		cls.driver.find_element_by_xpath("//*[@text='交易']").click()
+
+	def setup_method(self):
+		print("setup method 在每个测试用例执行之前执行一次")
+		self.driver=TestXueqiuAndroid.driver
+
+		self.driver.find_element_by_xpath("//*[@text='交易']").click()
+
+	def test_webview_simulator(self):
+		self.driver.find_element_by_accessibility_id('A股开户').click()
+		self.driver.find_element_by_accessibility_id('立即开户')
+		WebDriverWait(self.driver,20).until(EC.presence_of_element_located((MobileBy.ACCESSIBILITY_ID,'立即开户')))
+
+	def test_webview_B(self):
+		self.driver.find_element_by_accessibility_id('基金开户').click()
+		self.driver.find_element_by_accessibility_id('已有蛋卷基金账户登录').click()
+
+		self.driver.find_element_by_xpath('//*[@text="请输入手机号" and @instance="0"]').send_keys("12312")
+		self.driver.find_element_by_xpath('//*[@text="请输入6位验证码" and @instance="1"]').send_keys("123")
+		self.driver.find_element_by_accessibility_id('安全登录').click()
+
+	def teardown_method(self):
+		self.driver.back()
+		self.driver.back()
+
+	@classmethod
+	def install_app(cls) -> WebDriver:
+		caps = {}
+		#如果有必要，进行第一次安装
+		#caps["app] = ''
+		caps["platformName"] = "android"
+		caps["deviceName"] = "hogwarts"
+		caps["appPackage"] = "com.xueqiu.android"
+		caps["appActivity"] = ".view.WelcomeActivityAlias"
+		caps["autoGrantPermissions"] = "true"
+		#解决第一次启动
+		# caps["noReset"] = True
+		driver = webdriver.Remote("http://localhost:4723/wd/hub", caps)
+		driver.implicitly_wait(10)
+		return driver
+
+	@classmethod
+	def restart_app(cls) -> WebDriver:
+		caps = {}
+		caps["platformName"] = "android"
+		caps["deviceName"] = "hogwarts"
+		caps["appPackage"] = "com.xueqiu.android"
+		caps["appActivity"] = ".view.WelcomeActivityAlias"
+		# caps["autoGrantPermissions"] = "true"
+		#为了更快的启动，并保留之前的数据，从而保存上一个case执行后的状态
+		caps["noReset"] = True
+		driver = webdriver.Remote("http://localhost:4723/wd/hub", caps)
+		driver.implicitly_wait(10)
+		return driver
+
