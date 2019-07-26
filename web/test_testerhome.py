cb0fb2bc8cdf79efdc12023eb8763c7e714bab33
Natumeme <1466510866@qq.com>
2019-07-26 15:52:05 +0800

    add

--- web/test_testerhome.py
+++ web/test_testerhome.py
@@ -1,12 +1,18 @@
 #!/usr/bin/env python
 #-*-coding:utf-8-*-
+import json
 
 import time
 from selenium import webdriver
+from selenium.webdriver import DesiredCapabilities
+
 
 class TestTesterhome(object):
 	def setup(self):
-		self.driver = webdriver.Chrome()
+		options = webdriver.ChromeOptions()
+		# self.driver = webdriver.Chrome(options=options)
+		self.driver = cwebdriver.Remote(desired_capabilities=DesiredCapabilities.CHROME)
+
 		self.driver.implicitly_wait(10)
 		self.driver.get("https://testerhome.com/")
 
@@ -25,6 +31,11 @@ class TestTesterhome(object):
 		#执行JS的命令
 		raw = self.driver.execute_script("return JSON.stringify(window.performance.timing)")
 		print(raw)
+		print(json.dumps(json.loads(raw),indent=4))
+
+	def test_execute(self):
+		self.driver.execute("getXXX",params={"x":1,"y":2})
+
 
 	def teardown(self):
 		time.sleep(2)
