8a9f34c263d5d71045d21da2f69f22e190f0ec99
Natumeme <1466510866@qq.com>
2019-07-29 07:36:49 +0800

    add scree

---
+++ page_object/testcase/test_login.py
@@ -0,0 +1,23 @@
+#!usr/bin/env python
+#-*- coding:utf-8 -*-
+from page_object.page.App import App
+
+
+class TestLogin(object):
+	@classmethod
+	def setup_class(cls):
+		cls.loginPage=App.main().gotoProfile().gotoLogin()
+
+	def setup_method(self):
+		pass
+
+	def test_login_password(self):
+		#手机号错
+		self.loginPage.loginBypassword("12345678901","123456")
+		assert "手机号码" in self.loginPage.getErrorMsg()
+
+		#密码错
+		self.loginPage.loginBypassword("12345678901", "12345e6")
+		assert "密码" in self.loginPage.getErrorMsg()
+	def teardown_method(self):
+		pass
