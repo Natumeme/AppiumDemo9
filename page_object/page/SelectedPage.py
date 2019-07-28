d6df9af623825dbbb65425000a7e7b10a5e543a3
Natumeme <1466510866@qq.com>
2019-07-28 10:40:08 +0800

    update

--- page_object/page/SelectedPage.py
+++ page_object/page/SelectedPage.py
@@ -7,9 +7,11 @@ class SelectedPage(object):
 	def addDefault(self):
 		return self
 
-	def getPriceByName(self,name):
+	def getPriceByName(self,name) -> float:
 		#todo:
-		AndroidClient.driver\
-			.find_element_by_xpath("//*[contains(@resource-id,'stockName') and @text='"+name+"']"+
-		     "/../*[contains(@resource-id,'currentPrice')]").text
-		return price
+		price = AndroidClient.driver\
+			.find_element_by_xpath("//*[contains(@resource-id, 'stockName') and @text='%s']" %name +
+             "/../../..//*[contains(@resource-id, 'currentPrice')]").text
+		return float(price)
+
+	def getPrice
