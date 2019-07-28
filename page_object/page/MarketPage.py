0e0a66252a758a67a6df67449f2496738c8c3d08
Natumeme <1466510866@qq.com>
2019-07-28 10:51:22 +0800

    update

---
+++ page_object/page/MarketPage.py
@@ -0,0 +1,17 @@
+#!usr/bin/env python
+#-*- coding:utf-8 -*-
+from page_object.driver.AndroidClient import AndroidClient
+
+
+class SelectedPage(object):
+	def addDefault(self):
+		return self
+
+	def getPriceByName(self,name) -> float:
+		price = AndroidClient.driver\
+			.find_element_by_xpath("//*[contains(@resource-id, 'stockName') and @text='%s']" %name +
+             "/../../..//*[contains(@resource-id, 'currentPrice')]").text
+		return float(price)
+
+	def getMarketPrice(self):
+		price = AndroidClient.driver.find_element_by_xpath("//*[contains(@resource-id,'index_name') and @instance='13']/..//*[contains(@resource-id,'index_price') and @instance='12']")
