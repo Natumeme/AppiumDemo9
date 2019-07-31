8a4625fe62d30e58ccc861466dede48abb5da5e6
Natumeme <1466510866@qq.com>
2019-08-01 00:50:41 +0800

    update

--- page_object/testcase/test_login.py
+++ page_object/testcase/test_login.py
@@ -1,25 +1,25 @@
 #!usr/bin/env python
 #-*- coding:utf-8 -*-
 from page_object.page.App import App
+import pytest
 
 
 class TestLogin(object):
 	@classmethod
 	def setup_class(cls):
-		cls.loginPage=App.main().gotoProfile().gotoLogin()
+		cls.profilePage=App.main().gotoProfile()
 
 	def setup_method(self):
-		pass
+		self.loginPage=self.profilePage.gotoLogin()
 
-	def test_login_password(self):
+	@pytest.mark.parametrize("user,pw,msg",[
+		("1500000","111111","手机号码"),
+		("15600534760","111111","密码错误")
+	])
+	def test_login_password(self,user,pw,msg):
 		#手机号错误
-		self.loginPage.loginBypassword("123456789012","123456")
-		assert "手机号码" in self.loginPage.getErrorMsg()
-		self.loginPage.back()
-
-		#密码错误
-		# self.loginPage.loginBypassword("12345678901", "12345e6")
-		# assert "密码" in self.loginPage.getErrorMsg()
+		self.loginPage.loginBypassword(user,pw)
+		assert msg in self.loginPage.getErrorMsg()
 
 	def teardown_method(self):
-		pass
+		self.loginPage.back()
